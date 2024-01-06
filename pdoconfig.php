82492b93621b3e0193293e8ddd77d106d67d947c
Güney Berkay <101819906+guneyberkayates@users.noreply.github.com>
2024-01-06 13:36:56 +0300

    Update pdoconfig.php

--- pdoconfig.php
+++ pdoconfig.php
@@ -3,13 +3,9 @@
 	// veritabanı bağlantısı için gerekli parametreler
 	$host = "localhost";
 	$vt_adi = "engr372";
-<<<<<<< HEAD
 	$kullanici_adi = "abc";
 	$sifre = "abc";
-=======
-	$kullanici_adi = "root";
-	$sifre = "";
->>>>>>> eafa2b600d53c64cfd94f1ec6797451b740336bf
+	
 
 	try {
 		 $con = new PDO("mysql:host={$host};dbname={$vt_adi}", $kullanici_adi, $sifre,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
